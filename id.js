4c9b4b44a167e40b0ae5ca96fc5c6c6ba94ae1f1
horsemoose <mzacerclyaniy@gmail.com>
2025-12-04 21:56:03 +0100

    Add files via upload

---
+++ id.js
@@ -0,0 +1,56 @@
+var time = document.querySelector(".time_text");
+var params = new URLSearchParams(window.location.search);
+
+
+
+
+
+function pasek(){
+      document.querySelector(".bottom_bar").style.height = "70px"
+}
+
+let webManifest = {
+  "name": "",
+  "short_name": "",
+  "theme_color": "#f5f6fb",
+  "background_color": "#f5f6fb",
+  "display": "standalone"
+};
+
+window.addEventListener(
+  "touchmove",
+  function (event) {
+    if (event.scale !== 1) {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+    }
+  },
+  { passive: false }
+);
+
+let manifestElem = document.createElement('link');
+manifestElem.setAttribute('rel', 'manifest');
+manifestElem.setAttribute('href', 'data:application/manifest+json;base64,' + btoa(JSON.stringify(webManifest)));
+document.head.prepend(manifestElem);
+
+
+
+
+
+
+var options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+var date = new Date();
+document.querySelector(".bottom_update_value").innerHTML = date.toLocaleDateString("pl-PL", options);
+
+setClock();
+function setClock() {
+  date = new Date();
+  time.innerHTML = "Czas: " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " " + date.toLocaleDateString("pl-PL", options);
+  delay(1000).then(() => {
+    setClock();
+  })
+}
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
